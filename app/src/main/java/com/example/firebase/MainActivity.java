e1c13766bdf527ac63b4d6b73a5de0dc087dba78
merajuni7ed <merajmusavi1243m@gmail.com>
2023-02-23 22:50:35 +0330

    update

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("iran")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                   if (task.isSuccessful()){
+                       Toast.makeText(MainActivity.this, "good job :)", Toast.LENGTH_SHORT).show();
+                   }else
+                       Toast.makeText(MainActivity.this, "unfortunately failed :+(", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
